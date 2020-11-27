f2cc16d7d09fa47e4a770f17948621d7bc94a6a0
Supriya Khadka <075bct090.supriya@pcampus.edu.np>
2020-11-27 23:53:59 +0545

    Information Cards added

--- Source.cpp
+++ Source.cpp
@@ -3,11 +3,12 @@
 #include <string>
 #include <algorithm>
 #include<time.h>
-//#include <chrono>
-//#include <thread>
+#include <chrono>
+#include <thread>
 
 using namespace std;
-//using namespace std::chrono_literals;
+using namespace std::chrono_literals;
+
 //Initial Declarations of SFML components
 sf::Font font, openFont;
 sf::Event event;
@@ -16,7 +17,8 @@ sf::Color color(181, 101, 29);
 sf::RenderWindow renderWindow(sf::VideoMode(1000, 900), "Arthur- The Gladiator (Level 3)");
 
 std::vector<int> randomInts;
-int playerMoves = 20, randomNum, randomNumIndex, flag = 0, gameWon = 0, noMoves = 0;
+int playerMoves = 20, randomNum, randomNumIndex, flag = 0;
+bool gameWon = 0, noMoves = 0, noEnoughMoves = 0, giveUp = 0;
 void gameOver();
 void setVector(std::vector<int>);
 
@@ -250,7 +252,6 @@ void setVector(std::vector<int> randomList)
 {
 	//sf::RenderWindow renderWindow(sf::VideoMode(1000, 900), "Arthur- The Gladiator (Level 3)");
 	sf::RectangleShape selectSquare(sf::Vector2f(85, 85));
-
 	selectSquare.setFillColor(sf::Color::Transparent);
 	selectSquare.setOutlineColor(sf::Color::White);
 	selectSquare.setOutlineThickness(3);
@@ -266,19 +267,17 @@ void setVector(std::vector<int> randomList)
 	{
 		while (renderWindow.pollEvent(event))
 		{
-
 			if (event.type == sf::Event::EventType::Closed)
 				renderWindow.close();
 			if (playerMoves <= 0)
 			{
 				noMoves = 1;
-				gameOver();
 			}
 			if (event.type == sf::Event::EventType::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
 			{
 				int X = event.mouseButton.x;
 				int Y = event.mouseButton.y;
-				if (playerMoves > 0)
+				if (playerMoves >= 10)
 				{
 					if (X > 470 && X < 730 && Y > 50 && Y < 80)
 					{
@@ -296,10 +295,20 @@ void setVector(std::vector<int> randomList)
 						playerMoves -= 10;
 					}
 				}
+				else
+				{
+					noEnoughMoves = 1;
+				}
+				if (X > 50 && X < 180 && Y>50 && Y < 80)
+				{
+					giveUp = 1;
+					noEnoughMoves = 0;
+				}
 			}
 
 			if (event.type == sf::Event::EventType::KeyPressed)
 			{
+				noEnoughMoves = 0;
 				switch (event.key.code)
 				{
 				case sf::Keyboard::Left:
@@ -419,14 +428,24 @@ void setVector(std::vector<int> randomList)
 		sf::RectangleShape dSortButton(sf::Vector2f(250, 30));
 		dSortButton.setFillColor(color);
 		dSortButton.setPosition(sf::Vector2f(475, 50));
+		dSortButton.setOutlineThickness(2);
+		dSortButton.setOutlineColor(sf::Color::White);
 
 		sf::RectangleShape aSortButton(sf::Vector2f(250, 30));
 		aSortButton.setFillColor(color);
 		aSortButton.setPosition(sf::Vector2f(735, 50));
+		aSortButton.setOutlineThickness(2);
+		aSortButton.setOutlineColor(sf::Color::White);
 
 		sf::RectangleShape movesButton(sf::Vector2f(120, 30));
 		movesButton.setFillColor(color);
 		movesButton.setPosition(sf::Vector2f(865, 100));
+
+		sf::RectangleShape giveUpButton(sf::Vector2f(120, 30));
+		giveUpButton.setFillColor(color);
+		giveUpButton.setPosition(sf::Vector2f(50, 50));
+		giveUpButton.setOutlineThickness(2);
+		giveUpButton.setOutlineColor(sf::Color::White);
 
 		sf::RectangleShape princessInfoBox(sf::Vector2f(230, 30));
 		princessInfoBox.setFillColor(color);
@@ -441,10 +460,6 @@ void setVector(std::vector<int> randomList)
 		congratulationsBorder.setPosition(sf::Vector2f(195, 380));
 		congratulationsBorder.setOutlineThickness(3);
 		congratulationsBorder.setOutlineColor(sf::Color::White);
-
-		sf::Text congratulationsText("Congratulations! You won with " + std::to_string(playerMoves) + " moves remaining!",font, 20);
-		congratulationsText.setFillColor(sf::Color::White);
-		congratulationsText.setPosition(sf::Vector2f(225, 420));
 
 		sf::Text princessInfoText("Princess is in Box No. " + std::to_string(randomNum), font, 16);
 		princessInfoText.setFillColor(sf::Color::White);
@@ -465,6 +480,30 @@ void setVector(std::vector<int> randomList)
 		sf::Text playerMovesText(std::to_string(playerMoves), font, 16);
 		playerMovesText.setPosition(sf::Vector2f(950, 105));
 		playerMovesText.setFillColor(sf::Color::White);
+
+		sf::Text giveUpButtonText("Give Up", font, 16);
+		giveUpButtonText.setPosition(sf::Vector2f(75, 55));
+		giveUpButtonText.setFillColor(sf::Color::White);
+
+		sf::Text congratulationsText("Congratulations! You won with " + std::to_string(playerMoves) + " moves remaining!", font, 20);
+		congratulationsText.setFillColor(sf::Color::White);
+		congratulationsText.setPosition(sf::Vector2f(225, 420));
+
+		sf::Text sorryText("Sorry, you are out of moves. Game Over!", font, 20);
+		sorryText.setFillColor(sf::Color::White);
+		sorryText.setPosition(sf::Vector2f(275, 420));
+
+		sf::Text noEnoughMovesText1("You don't have enough moves for sorting!", font, 20);
+		noEnoughMovesText1.setFillColor(sf::Color::White);
+		noEnoughMovesText1.setPosition(sf::Vector2f(275, 405));
+
+		sf::Text noEnoughMovesText2("Use the remaining moves elsewhere or give up!", font, 20);
+		noEnoughMovesText2.setFillColor(sf::Color::White);
+		noEnoughMovesText2.setPosition(sf::Vector2f(250, 435));
+
+		sf::Text giveUpText("You gave up with " + std::to_string(playerMoves) + " moves remaining!", font, 20);
+		giveUpText.setFillColor(sf::Color::White);
+		giveUpText.setPosition(sf::Vector2f(285, 420));
 
 		int coordY = 200;
 		int a = 0;
@@ -501,22 +540,43 @@ void setVector(std::vector<int> randomList)
 		renderWindow.draw(aSort);
 		renderWindow.draw(movesButton);
 		renderWindow.draw(movesText);
+		renderWindow.draw(giveUpButton);
+		renderWindow.draw(giveUpButtonText);
 		renderWindow.draw(princessInfoBox);
 		renderWindow.draw(princessInfoText);
 		renderWindow.draw(playerMovesText);
 		renderWindow.draw(selectSquare);
-		if (gameWon)
+		if (noMoves || gameWon || noEnoughMoves || giveUp)
 		{
-			renderWindow.draw(princessSprite);
+			if (gameWon)
+			{
+				renderWindow.draw(princessSprite);
+			}
 			renderWindow.draw(congratulationsBox);
 			renderWindow.draw(congratulationsBorder);
-			renderWindow.draw(congratulationsText);
+			if (noMoves)
+			{
+				renderWindow.draw(sorryText);
+			}
+			if (gameWon)
+			{
+				renderWindow.draw(congratulationsText);
+			}
+			if (noEnoughMoves)
+			{
+				renderWindow.draw(noEnoughMovesText1);
+				renderWindow.draw(noEnoughMovesText2);
+			}
+			if (giveUp)
+			{
+				renderWindow.draw(giveUpText);
+			}
 		}
 		renderWindow.display();
-		//if (gameWon)
-		//{
-			//gameOver();
-		//}
+		if (gameWon || noMoves || giveUp)
+		{
+			gameOver();
+		}
 	}
 }
 
@@ -557,8 +617,18 @@ int main()
 
 void gameOver()
 {
-	//sf::RenderWindow renderWindow(sf::VideoMode(1000, 900), "Arthur- The Gladiator (Level 3)");
-	std::cout << "You are out of moves. Game Over!" << std::endl;
-	//std::this_thread::sleep_for(2s);
+	if (noMoves)
+	{
+		std::cout << "You are out of moves. Game Over!" << std::endl;
+	}
+	if (gameWon)
+	{
+		std::cout << "Congratulations! You won with " << playerMoves << " moves remaining!" << std::endl;
+	}
+	if (giveUp)
+	{
+		std::cout << "You gave up with " << playerMoves << " moves remaining!" << std::endl;
+	}
+	std::this_thread::sleep_for(1.5s);
 	renderWindow.close();
 }
